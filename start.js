6d1a25a33157c230344f1f1150ab1fa776c70592
JMan <justinvang07@gmail.com>
2021-02-06 16:29:39 -0600

    created start function with inquirer prompt

--- start.js
+++ start.js
@@ -1,16 +1,13 @@
 var mysql = require('mysql');
-var inquirer = require('inquirere');
+var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
     host: "localhost",
   
-    // Your port; if not 3306
     port: 3306,
   
-    // Your username
     user: "root",
   
-    // Your password
     password: "rootroot",
     database: "employee_trackerDB"
   });
@@ -18,5 +15,27 @@ var connection = mysql.createConnection({
   connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    mainMenu();
+    start();
   });
+
+  function start() {
+      inquirer
+        .prompt({
+            name: "addViewUpdate",
+            type: "list",
+            message: "Would you like to add, view, or update an employee|department|role?",
+            choices: ["Add", "View", "Update"]
+        })
+        .then(function(answer) {
+            if (answer.addViewUpdate === "Add") {
+                addMenu();
+            } else if(answer.addViewUpdate === "View") {
+                viewMenu();
+            } else if(answer.addViewUpdate === "Update") {
+                updateMenu();
+            } else {
+                connection.end();
+            }
+        });
+  }
+  
