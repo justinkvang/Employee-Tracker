454d011d5396fc88283908c5e8140ff4b4bf8937
JMan <justinvang07@gmail.com>
2021-02-07 12:52:06 -0600

    updates schema and seed

--- schema/schema.sql
+++ schema/schema.sql
@@ -7,7 +7,6 @@ USE employee_trackerDB;
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30)
- 
 );
 
 CREATE TABLE role (
--- schema/seed.sql
+++ schema/seed.sql
@@ -10,31 +10,31 @@ INSERT INTO department (name)
 VALUE ("Legal");
 
 INSERT INTO role (title, salary, department_id)
-VALUE ("Lead Engineer", 150000, 2);
+VALUE ("Lead Engineer", 91000, 2);
 INSERT INTO role (title, salary, department_id)
-VALUE ("Legal Team Lead", 250000, 4);
+VALUE ("Legal Team Lead", 127000, 4);
 INSERT INTO role (title, salary, department_id)
-VALUE ("Accountant", 125000, 3);
+VALUE ("Accountant", 86000, 3);
 INSERT INTO role (title, salary, department_id)
-VALUE ("Sales Lead", 100000, 1);
+VALUE ("Sales Lead", 115000, 1);
 INSERT INTO role (title, salary, department_id)
-VALUE ("Salesperson", 80000, 1);
+VALUE ("Salesperson", 73000, 1);
 INSERT INTO role (title, salary, department_id)
-VALUE ("Software Engineer", 120000, 2);
+VALUE ("Software Engineer", 129000, 2);
 INSERT INTO role (title, salary, department_id)
-VALUE ("Lawyer", 190000, 4);
+VALUE ("Lawyer", 210000, 4);
 
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Jessica", "Haze", null, 1);
+VALUE ("Constance", "Wu", null, 1);
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Tiffany", "Patric", null, 2);
+VALUE ("Randall", "Park", null, 2);
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Mia","Lam",null,3);
+VALUE ("Daniel","Kim",null,3);
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Bently", "Lao", 1, 4);
+VALUE ("Bruce", "Lee", 1, 4);
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Chris", "Melby", 4, 5);
+VALUE ("Brenda", "Song", 4, 5);
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Jason", "Baker", 1, 6);
+VALUE ("Jamie", "Chung", 1, 6);
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Tom", "Nice", 2, 7);
+VALUE ("Lucy", "Liu", 2, 7);
